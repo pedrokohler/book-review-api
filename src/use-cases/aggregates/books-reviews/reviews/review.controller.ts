db5d8be3e0948ec332d02b587de1febb0545c85a
pedrokohler <pedrokohlerbh@gmail.com>
2022-01-15 12:57:31 -0300

    Add endpoint to add review

--- src/use-cases/aggregates/books-reviews/reviews/review.controller.ts
+++ src/use-cases/aggregates/books-reviews/reviews/review.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { IReviewData } from '../common/interfaces/review-data.interface';
+import { IReview } from '../common/interfaces/review.interface';
+import { ReviewService } from './review.service';
 
 @Controller('books/:bookId/reviews')
-export class ReviewController {}
+export class ReviewController {
+  constructor(private readonly reviewService: ReviewService) {}
+
+  @Post()
+  async createReview(
+    @Param('bookId') bookId: string,
+    @Body() data: IReviewData,
+  ): Promise<IReview> {
+    return await this.reviewService.createReview({ bookId, data });
+  }
+}
