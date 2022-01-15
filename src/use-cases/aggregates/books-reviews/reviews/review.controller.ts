c3605a41b7847fedc45b6704b4015a1925ea0f24
pedrokohler <pedrokohlerbh@gmail.com>
2022-01-15 15:35:17 -0300

    Add enpoints to get review sums and to delete reviews

--- src/use-cases/aggregates/books-reviews/reviews/review.controller.ts
+++ src/use-cases/aggregates/books-reviews/reviews/review.controller.ts
@@ -1,17 +1,32 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { IBookRatingsByAuthor } from '../common/interfaces';
 import { IReviewData } from '../common/interfaces/review-data.interface';
 import { IReview } from '../common/interfaces/review.interface';
 import { ReviewService } from './review.service';
 
-@Controller('books/:bookId/reviews')
+@Controller()
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
-  @Post()
+  @Post('books/:bookId/reviews')
   async createReview(
     @Param('bookId') bookId: string,
     @Body() data: IReviewData,
   ): Promise<IReview> {
     return await this.reviewService.createReview({ bookId, data });
+  }
+
+  @Get('books/reviews/sum')
+  async getSumOfRatingsGroupedByAuthor(): Promise<IBookRatingsByAuthor> {
+    return await this.reviewService.getSumOfRatingsGroupedByAuthor();
+  }
+
+  @Delete('books/:bookId/reviews/:reviewId')
+  async deleteReview(
+    @Param('bookId') bookId: string,
+    @Param('reviewId') reviewId: string,
+  ): Promise<boolean> {
+    // @todo: fix response payload
+    return await this.reviewService.deleteReview({ bookId, reviewId });
   }
 }
