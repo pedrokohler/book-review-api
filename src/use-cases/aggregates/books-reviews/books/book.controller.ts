30c5d7b02508e42fe47fcf8faf4d2fc30f5fcf6f
pedrokohler <pedrokohlerbh@gmail.com>
2022-01-15 12:40:59 -0300

    Create endpoint to get books grouped by fields

--- src/use-cases/aggregates/books-reviews/books/book.controller.ts
+++ src/use-cases/aggregates/books-reviews/books/book.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { IBook } from '../common/interfaces';
 import { BookService } from './book.service';
+import { GroupByOption } from './enums/group-by-option.enum';
 import { IBookDto } from './interfaces';
 
 @Controller('books')
@@ -10,5 +19,20 @@ export class BookController {
   @Post()
   async createBook(@Body() data: IBookDto): Promise<IBook> {
     return await this.bookService.createBook(data);
+  }
+
+  @Get()
+  async getAllBooksGrouped(@Query('group-by') groupBy: GroupByOption) {
+    switch (groupBy) {
+      case GroupByOption.GENRE:
+        return await this.bookService.getAllBooksGroupedByGenre();
+      case GroupByOption.GENRE_AND_RELEASE_DATE:
+        return await this.bookService.getAllBooksGroupedByGenreAndYear();
+      default:
+        throw new HttpException(
+          'Invalid groupBy value',
+          HttpStatus.BAD_REQUEST,
+        );
+    }
   }
 }
---
+++ src/use-cases/aggregates/books-reviews/books/enums/group-by-option.enum.ts
@@ -0,0 +1,4 @@
+export enum GroupByOption {
+  GENRE = 'genre',
+  GENRE_AND_RELEASE_DATE = 'genre_and_release_date',
+}
