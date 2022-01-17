33a9ce5ab0472fb7291d1933e4763f1de3e13b37
pedrokohler <pedrokohlerbh@gmail.com>
2022-01-17 08:40:58 -0300

    Add port environemtn varible to nest bootstrap

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setSwaggerApiDocumentation(app);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
